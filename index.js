cb534f89eb05f5e0ba9e2cadf252e6ad537239a8
Ilkka Kivistö <ilkka.kivisto@gmail.com>
2022-02-12 18:09:01 +0200

    Initial commit of backend

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const { json } = require('express')
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 const app = express()
 
 let persons = [
@@ -35,6 +36,7 @@ morgan.token('body', (req, res) => {
 
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+app.use(cors())
 
 app.get('/api/persons', (req, res) => {
   res.json(persons)
@@ -92,7 +94,7 @@ app.post('/api/persons', (req, res) => {
   res.json(person)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
