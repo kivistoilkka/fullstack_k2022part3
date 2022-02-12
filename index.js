46602f491dbfd4294f6f058638c03e71e77378de
Ilkka Kivistö <ilkka.kivisto@gmail.com>
2022-02-12 18:52:45 +0200

    Exercise 3.11: puhelinluettelo fullstack

--- index.js
+++ index.js
@@ -37,6 +37,7 @@ morgan.token('body', (req, res) => {
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 app.use(cors())
+app.use(express.static('build'))
 
 app.get('/api/persons', (req, res) => {
   res.json(persons)
