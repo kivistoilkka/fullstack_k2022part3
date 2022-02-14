9f7c88253ec0400f869094550dd3b87ecefac7e7
Ilkka Kivistö <ilkka.kivisto@gmail.com>
2022-02-14 17:47:15 +0200

    Exercise 3.19: puhelinluettelo ja tietokanta, step7

--- index.js
+++ index.js
@@ -56,16 +56,6 @@ app.delete('/api/persons/:id', (req, res, next) => {
 
 app.post('/api/persons', (req, res, next) => {
   const body = req.body
-  if (!body.name) {
-    return res.status(400).json({
-      error: 'name is missing'
-    })
-  }
-  if (!body.number) {
-    return res.status(400).json({
-      error: 'number is missing'
-    })
-  }
 
   const person = new Person({
     name: body.name,
@@ -80,14 +70,13 @@ app.post('/api/persons', (req, res, next) => {
 })
 
 app.put('/api/persons/:id', (req, res, next) => {
-  const body = req.body
+  const { name, number } = req.body
 
-  const person = {
-    name: body.name,
-    number: body.number,
-  }
-
-  Person.findByIdAndUpdate(req.params.id, person, { new: true })
+  Person.findByIdAndUpdate(
+    req.params.id,
+    { name, number },
+    { new: true, runValidators: true, context: 'query' }
+  )
     .then(updatedPerson => {
       res.json(updatedPerson)
     })
@@ -104,6 +93,8 @@ const errorHandler = (error, req, res, next) => {
   console.error(error.message)
   if (error.name === 'CastError') {
     return res.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return res.status(400).json({ error: error.message })
   }
   next(error)
 }
